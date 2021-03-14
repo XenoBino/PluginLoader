0f9e6eb1def1e6a45732bb37f198ea373bece1b1
Ahmed Al-Shoqanebi <meahmedshaqanbi@gmail.com>
2021-03-14 11:06:02 +0300

    Added build messages and suppressed cmake stdout

--- PluginLoader/build.sh
+++ PluginLoader/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
-mkdir Build/
-cmake . -BBuild/
-cmake --build Build/
+(mkdir -p Build/) && (cmake . -BBuild/ > /dev/null)\
+&& (cmake --build Build/ > /dev/null) &&\
+echo -e "\e[32mSuccefully built \e[33mPluginLoader \e[32min \e[33mBuild/libPluginLoader.so\e[0m" && \
+exit 0
+
+echo -e "\e[31mError: \e[0mFailed to build \e[33mPluginLoader\e[0m"
+exit 1
